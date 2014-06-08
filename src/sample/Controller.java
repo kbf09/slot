17bfd097ceb4031ba9e975c67e4fd847a46519b7
kbf09 <pexpex123@gmail.com>
2014-06-08 10:07:04 +0900

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Label t1;
+    @FXML
+    private Label t2;
+    @FXML
+    private Label t3;
+    Random rnd1 = new Random();
+
+
+    @FXML
+    public void ac1(ActionEvent event) {
+
+            t1.setText("" +rnd1.nextInt(10));
+
+    }
+    @FXML
+    private void ac2(ActionEvent event) {
+
+            t2.setText("" +rnd1.nextInt(10));
+
+    }
+    @FXML
+    private void ac3(ActionEvent event) {
+
+            t3.setText("" +rnd1.nextInt(10));
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {}
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("スロット");
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
